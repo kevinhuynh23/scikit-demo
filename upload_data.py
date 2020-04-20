4976bed87da111d4a7ed4caf6cd00db10cc57518
Kevin Huynh <kevhuyn@amazon.com>
2020-04-20 11:23:33 -0700

    Change upload path to match train

--- upload_data.py
+++ upload_data.py
@@ -4,6 +4,6 @@ import boto3
 s3_resource = boto3.resource('s3')
 
 BUCKET_NAME_SOURCE = os.environ.get('SRC_BKT_NAME')
-PREFIX = 'input/data/'
+PREFIX = 'input/data/training'
 
 s3_resource.Bucket(BUCKET_NAME_SOURCE).upload_file("data/iris.csv", PREFIX + 'iris.csv')
