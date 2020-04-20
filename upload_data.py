137da6d5822b724ca8a3cfb04069c13cb7a45bd0
Kevin Huynh <kevhuyn@amazon.com>
2020-04-20 11:02:04 -0700

    Initial commit

---
+++ upload_data.py
@@ -0,0 +1,9 @@
+import os
+import boto3
+
+s3_resource = boto3.resource('s3')
+
+BUCKET_NAME_SOURCE = os.environ.get('SRC_BKT_NAME')
+PREFIX = 'input/data/'
+
+s3_resource.Bucket(BUCKET_NAME_SOURCE).upload_file("data/iris.csv", PREFIX + 'iris.csv')
